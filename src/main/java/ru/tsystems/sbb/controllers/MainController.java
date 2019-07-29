76b09f8d5a758744d90819ed8c9bdbed9d6cb65c
ele-o-nora <elenet@gmail.com>
2019-07-29 23:08:43 +0300

    change requestmapping to getmapping

--- src/main/java/ru/tsystems/sbb/controllers/MainController.java
+++ src/main/java/ru/tsystems/sbb/controllers/MainController.java
@@ -2,7 +2,7 @@ package ru.tsystems.sbb.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 import ru.tsystems.sbb.services.view.ScheduleViewService;
 
@@ -12,7 +12,7 @@ public class MainController {
     @Autowired
     private ScheduleViewService viewService;
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public ModelAndView homePage() {
         return new ModelAndView("index", viewService.getStationsList());
     }
