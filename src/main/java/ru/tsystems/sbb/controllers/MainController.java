5773688fa1a3749844372951fbc04ccea8d4bf57
ele-o-nora <elenet@gmail.com>
2019-08-18 14:02:58 +0300

    minor fix

--- src/main/java/ru/tsystems/sbb/controllers/MainController.java
+++ src/main/java/ru/tsystems/sbb/controllers/MainController.java
@@ -3,18 +3,22 @@ package ru.tsystems.sbb.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 import ru.tsystems.sbb.model.dto.SignUpDto;
 import ru.tsystems.sbb.services.view.ScheduleViewService;
 
 @Controller
+@SessionAttributes({"ticketOrder", "transferTickets"})
 public class MainController {
 
     @Autowired
     private ScheduleViewService viewService;
 
     @GetMapping("/")
-    public ModelAndView homePage() {
+    public ModelAndView homePage(SessionStatus status) {
+        status.setComplete();
         return new ModelAndView("index", viewService.getStationsList());
     }
 
