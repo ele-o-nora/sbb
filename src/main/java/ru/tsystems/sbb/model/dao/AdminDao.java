3def5bb7091e8cc1013c3fc81196de260583734d
ele-o-nora <elenet@gmail.com>
2019-08-18 15:14:05 +0300

    javadoc work in progress

--- src/main/java/ru/tsystems/sbb/model/dao/AdminDao.java
+++ src/main/java/ru/tsystems/sbb/model/dao/AdminDao.java
@@ -17,18 +17,90 @@ import java.util.List;
  *  schedule, and pricing.
  */
 public interface AdminDao {
+
+    /**
+     * Adds new station to the database.
+     * @param station station to add to the database
+     */
     void add(Station station);
+
+    /**
+     * Adds new line-station relation to the database.
+     * @param lineStation line-station relation to be added to the database
+     */
     void add(LineStation lineStation);
+
+    /**
+     * Updates existing line-station relation.
+     * @param lineStation line-station relation to be updated
+     */
     void update(LineStation lineStation);
+
+    /**
+     * Adds new train model to the database.
+     * @param train train model to be added to the database
+     */
     void add(Train train);
+
+    /**
+     * Adds new route to the database.
+     * @param route route to be added to the database
+     */
     void add(Route route);
+
+    /**
+     * Adds new route-station relation to the database.
+     * @param routeStation route-station relation to be added to the database
+     */
     void add(RouteStation routeStation);
+
+    /**
+     * Adds new scheduled journey to the database.
+     * @param journey journey to be added to the database
+     */
     void add(Journey journey);
+
+    /**
+     * Adds new scheduled stop to the database.
+     * @param scheduledStop stop to be added to the database
+     */
     void add(ScheduledStop scheduledStop);
+
+    /**
+     * Removes current route-station relations from the database.
+     * @param route route for which relations are to be removed
+     */
     void cleanRouteStopPattern(Route route);
+
+    /**
+     * Gets from database all existing train models.
+     * @return complete list of Train
+     */
     List<Train> getAllTrainModels();
+
+    /**
+     * Gets from database train model with specific id.
+     * @param trainId id indication which train model will be returned
+     * @return Train identified by specified id
+     */
     Train getTrainById(int trainId);
+
+    /**
+     * Adds new distance between stations to the database.
+     * @param stationsDistance istance between stations to be added
+     */
     void add(StationsDistance stationsDistance);
+
+    /**
+     * Removes from database distance between specific stations.
+     * @param s1 first station for which distance is to be removed
+     * @param s2 second station for which distance is to be removed
+     */
     void deleteDistance(Station s1, Station s2);
+
+    /**
+     * Adds new tariff to the database.
+     * @param tariff tariff to be added to the database
+     */
     void add(Tariff tariff);
 }
